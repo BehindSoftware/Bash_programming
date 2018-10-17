bd567aba3ec90d4d177d49e178f2e40c5d82d66d
Behind Software <37362311+BehindSoftware@users.noreply.github.com>
2018-10-17 10:54:55 +0300

    Update myprog2.sh

--- myprog2.sh
+++ myprog2.sh
@@ -8,9 +8,15 @@ echo "Enter a sequence of numbers followed by "end""	#notification for users
 while [ "$input" != "end" ]	#control to entering end input
 do
 	read input		#take input from user
-	if [ $Maximum -lt $input ]	#if input greater than our maximum value
+	if [ "$input" != "end" ] && [ $Maximum -lt $input ]	#if input is not end and greater than our maximum value
 	then
 		Maximum=$input		#do it new maximum value
 	fi
 done
-echo "Maximum:$Maximum"			#debug-print to maximum value
+
+if [ $Maximum -eq 0 ]		#control to having any number input
+then
+	echo "You don't enter any numbers to determine maximum of them."
+else
+	echo "Maximum:$Maximum"			#debug-print to maximum values
+fi
